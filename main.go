4dc223a908a17374cfae4b48cf5d3aacb518c803
jonathanpatta <jonathan.patta@gmail.com>
2025-06-29 16:04:33 -0700

    defining structs

--- main.go
+++ main.go
@@ -1,5 +1,47 @@
 package main
 
+import (
+	"sync"
+)
+
+type Player struct {
+	ID        string
+	Hand      []*Card
+	Stack     []*Card
+	Graveyard []*Card
+}
+type Card struct {
+	ID        string
+	Type      string
+	Owner     string
+	Abilities []Ability
+}
+
+type Ability struct {
+	Type    string
+	Trigger string
+	Effect  string
+	Target  string
+}
+
+type GameState struct {
+	Players    map[string]*Player
+	Turn       string
+	EventQueue []Event
+	mutex      sync.RWMutex
+}
+
+type Event struct {
+	ID     string
+	Type   string
+	Player string
+	Depth  int
+}
+
+type GameEngine struct {
+	state *GameState
+}
+
 func main() {
 
 }
