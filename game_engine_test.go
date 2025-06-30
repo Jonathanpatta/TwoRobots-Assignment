17bf032add58459a0214ae6b74e056705a8e0d10
jonathanpatta <jonathan.patta@gmail.com>
2025-06-30 11:19:01 -0700

    Create Basic Test

--- game_engine_test.go
+++ game_engine_test.go
@@ -2,5 +2,69 @@ package main
 
 import "testing"
 
+func InitializeTestGameScenario() (*GameEngine, *Card) {
+	card1 := &Card{
+		ID:    "1",
+		Owner: "player1",
+		Abilities: []Ability{
+			{
+				Type:   "instant",
+				Effect: "destroy_stack",
+				Target: "enemy_stack",
+			},
+		},
+	}
+
+	card2 := &Card{
+		ID:    "2",
+		Owner: "player2",
+		Abilities: []Ability{
+			{
+				Type:    "triggered",
+				Trigger: "on_destroy",
+				Effect:  "salvage_self",
+				Target:  "player_hand",
+			},
+		},
+	}
+
+	ge := &GameEngine{
+		maxDepth: 3,
+		state: &GameState{
+			Players:    make(map[string]*Player),
+			EventQueue: make([]Event, 0),
+		},
+	}
+
+	ge.state.Players["player1"] = &Player{
+		ID:        "player1",
+		Hand:      make([]*Card, 0),
+		Stack:     make([]*Card, 0),
+		Graveyard: make([]*Card, 0),
+	}
+
+	ge.state.Players["player1"].Hand = append(ge.state.Players["player1"].Hand, card1)
+
+	ge.state.Players["player2"] = &Player{
+		ID:        "player2",
+		Hand:      make([]*Card, 0),
+		Stack:     make([]*Card, 0),
+		Graveyard: make([]*Card, 0),
+	}
+	ge.state.Players["player2"].Stack = append(ge.state.Players["player2"].Stack, card2)
+	ge.state.Players["player2"].Stack = append(ge.state.Players["player2"].Stack, card2)
+	ge.state.Players["player2"].Stack = append(ge.state.Players["player2"].Stack, card2)
+	ge.state.Players["player2"].Stack = append(ge.state.Players["player2"].Stack, card2)
+	ge.state.Players["player2"].Stack = append(ge.state.Players["player2"].Stack, card2)
+	return ge, card1
+}
+
 func TestGameEngine_ExecuteAbility(t *testing.T) {
+	ge, card1 := InitializeTestGameScenario()
+
+	err := ge.ExecuteAbility(card1, card1.Abilities[0], 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ge.PrintGameState()
 }
